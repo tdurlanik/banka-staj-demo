5128fcf5751804defd59222d7b95e48eda4eb485
Tayfur Durlanık <durlaniktayfur@gmail.com>
2026-07-31 11:00:18 +0300

    Sahte test betigi ve Test asamasi eklendi

---
+++ test_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Birim testleri baslatiliyor..."
+sleep 1
+echo "Matematiksel hesaplama fonksiyonları kontrol ediliyor... OK"
+sleep 1
+echo "Asenkron veri çekme testleri yapılıyor... OK"
+sleep 1
+echo "TUM TESTLER BASARILI!"
+exit 0
